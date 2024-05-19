50a22bc2d4db7ce4f10cb2a9b07c91ee1c2aef8a
Huber Soto <104796746+hubers0@users.noreply.github.com>
2024-05-19 22:25:11 +0000

    todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
 import React, { useState } from "react";
 
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
 
+//create your first component
 const Home = () => {
-	const [ inputValue, SetInputValue ] =useState("")
-	const [ todos, setTodos ] =useState([])
+	const [inputValue, setInputValue] = useState("")
+	const [tasks, setTasks] = useState([]); 
 	return (
-		<div className="container">
-			<h1>My Todos{inputValue}</h1>
-			<ul>
-				<li><input 
-				type="text" 
-				onChange={(e) => SetInputValue(e.target.value)}
-				value={inputValue}
-				placeholder="What do you need to do?"></input>
-				</li>
-				<li>Make the bed <i class="fas fa-trash-alt"></i></li>
-				<li>Walk the dog <i class="fas fa-trash-alt"></i></li>
-				<li>Wash my hands <i class="fas fa-trash-alt"></i></li>
-				<li>Eat <i class="fas fa-trash-alt"></i></li>
-			</ul>
-			<dir>23 tasks</dir>
-		</div>
-	)
-	}
+		<React.Fragment>
+				<h1>My Todos</h1>
+			<div className="container">
+					<div className="list">
+						<ul>
+							<li key={tasks.id}>
+								<input 
+								type="text" 
+								placeholder="Write your task here"
+								onChange={(event) => {
+									setInputValue(event.target.value)
+								}}
+								value={inputValue} 
+								onKeyDown={(event) => { 
+									if (event.key == "Enter") {
+										if (inputValue.trim() == "") {
+											alert("Do not send empty spaces")
+										} else {
+										setTasks([...tasks, inputValue]); 
+										setInputValue("") 
+										  }	
+									} 
+								}}
+								/>
+							</li>
+							{tasks.map((task, index) => (
+							<li key={index} className="containerLi">
+								{task} 
+								<button
+								type="reset"
+								onClick={(event) => {
+									const deleteTask = tasks.filter((list) => list !== task)
+									setTasks(deleteTask)
+								}}
+								>
+									X
+								</button>
+							</li>
+							))}
+						</ul>
+					</div>
+					<p>
+					{tasks.length + " Tasks"}
+					</p>			
+			</div>
+			<div className="stick"></div>
+			<div className="stick2"></div>
+		</React.Fragment>
+	);
+};
+
 export default Home;
